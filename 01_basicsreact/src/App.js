7b6d9f20f523c15267fb6545124ff2f78bd5818f
travelblog666 <145565199+travelblog666@users.noreply.github.com>
2024-04-24 12:45:46 +0530

    updated notes

--- 01_basicsreact/src/App.js
+++ 01_basicsreact/src/App.js
@@ -1,7 +1,11 @@
-
+import Chai from "./chai";
 function App() {
   return (
-   <h1>Chai aur React | Prajwal A learnt how to setup react projects</h1>
+    <>
+    <Chai/>
+    <h1>Chai aur React | Prajwal A learnt how to setup react projects</h1> 
+    </>
+    
   );
 }
 
---
+++ 01_vitereact/src/chai.jsx
@@ -0,0 +1,14 @@
+function Chai(){
+
+    return(
+        //this is called fragmentation
+        <>
+        
+        <h1>Chai aur React</h1>
+        <h2>Chai aur Vite</h2>
+        </>
+        
+    )
+}
+
+export default Chai
